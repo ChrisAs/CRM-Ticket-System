163f529ca062c8f2501e1d4ccbc04a623cbc6b3b
Chris <zari87@live.com>
2020-10-28 23:13:05 -0400

    added entry page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-
+import {Button} from 'react-bootstrap'
+import { Entry } from './page/Entry/entry.js';
 function App() {
   return (
     <div className="App">hello
-     
+    <Entry/>
+     <Button>Test button</Button>
     </div>
   );
 }
---
+++ src/page/Entry/entry.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './entry.style.css'
+ export const Entry = () => {
+    return (
+        <div className="entry-page bg-info">
+          Entry page  
+        </div>
+    )
+}
