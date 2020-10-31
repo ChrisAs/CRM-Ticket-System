b009e1381536cde650b604ec3d31111beaf6fde2
Chris <zari87@live.com>
2020-10-31 00:30:33 -0400

    Added responsivness to navbar

--- src/layout/partials/Header.comp.js
+++ src/layout/partials/Header.comp.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { Navbar, Nav } from "react-boostrap";
-import { logo } from "../../assets/img/logo.png";
+import { Navbar, Nav, NavbarBrand } from "react-bootstrap";
+import logo from "../../assets/img/logo.png";
 export const Header = () => {
   return (
-    <Navbar collapseOnSelect variant="dark" bg="info" expand="md">
+    <Navbar collapseOnSelect bg="info" variant="dark" expand="md">
       <Navbar.Brand>
         <img src={logo} alt="logo" width="50px" />
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav>
+        <Nav className="ml-auto">
           <Nav.Link href="/dashboard">Dashboard</Nav.Link>
           <Nav.Link href="/dashboard">Tickets</Nav.Link>
           <Nav.Link href="/dashboard">Logout</Nav.Link>
