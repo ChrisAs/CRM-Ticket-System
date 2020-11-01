7d024f5b6121ec59fb6d69599e723f6cad4eb28f
Chris <zari87@live.com>
2020-11-01 14:30:55 -0500

    Added: New ticket form

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { Button } from "react-bootstrap";
 import { Entry } from "./pages/Entry/Entry.page.js";
 import { Dashboard } from "./pages/dashboard/Dasboard.page";
 import { DefaultLayout } from "./layout/DefaultLayout";
+import { AddTicket } from "./pages/new-ticket/AddTicket.page";
 function App() {
   return (
     <div className="App">
       <Entry />
-
       <DefaultLayout>
         <Dashboard />
+        <AddTicket />
       </DefaultLayout>
     </div>
   );
--- src/components/add-ticket-form/AddTicketForm.comp.js
+++ src/components/add-ticket-form/AddTicketForm.comp.js
@@ -1,5 +1,50 @@
 import React from "react";
-
-export const AddTicketForm = () => {
-  return <div>form</div>;
+import { Jumbotron, Form, Button, Row, Col } from "react-bootstrap";
+export const AddTicketForm = ({ handleOnSubmit, handleOnChange }) => {
+  return (
+    <Jumbotron>
+      <Form autoComplete="off" onSubmit={handleOnSubmit}>
+        <Form.Group as={Row}>
+          <Form.Label column sm={3}>
+            Subject
+          </Form.Label>
+          <Col sm={9}>
+            <Form.Control
+              name="subject"
+              // value={email}
+              onChange={handleOnChange}
+              placeholder="Subject"
+              required
+            />
+          </Col>
+        </Form.Group>
+        <Form.Group as={Row}>
+          <Form.Label column sm={3}>
+            Issue Found
+          </Form.Label>
+          <Col sm={9}>
+            <Form.Control
+              type="date"
+              name="issueDate"
+              onChange={handleOnChange}
+              required
+            />
+          </Col>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            as="textarea"
+            name="details"
+            rows="5"
+            onChange={handleOnChange}
+            required
+          />
+        </Form.Group>
+        <Button type="submit" block variant="info">
+          Login
+        </Button>
+      </Form>
+    </Jumbotron>
+  );
 };
