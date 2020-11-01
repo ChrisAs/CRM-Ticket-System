ba7662ea5d65a5bce25e5208a79d53f93ef45670
Chris <zari87@live.com>
2020-11-01 14:49:45 -0500

    Fix: Frmdata

--- src/pages/new-ticket/AddTicket.page.js
+++ src/pages/new-ticket/AddTicket.page.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { PageBreadcrumb } from "../../components/breadcrumb/Breadcrumb.comp";
 import { AddTicketForm } from "../../components/add-ticket-form/AddTicketForm.comp";
 export const AddTicket = () => {
   const [frmData, setfrmData] = useState(initialFrmDt);
+  useEffect(() => {}, [frmData]);
   const initialFrmDt = {
     subject: "",
     issueDate: "",
@@ -11,8 +12,9 @@ export const AddTicket = () => {
   };
   const handleOnChange = (e) => {
     const { name, value } = e.target;
+
     setfrmData({
-      ...initialFrmDt,
+      ...frmData,
       [name]: value,
     });
   };
