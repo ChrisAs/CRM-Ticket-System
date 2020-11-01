08080263ff5402342697b1347f24782a9ec1a155
Chris <zari87@live.com>
2020-11-01 15:24:22 -0500

    Added Ticketlists to App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Entry } from "./pages/Entry/Entry.page.js";
 import { Dashboard } from "./pages/dashboard/Dasboard.page";
 import { DefaultLayout } from "./layout/DefaultLayout";
 import { AddTicket } from "./pages/new-ticket/AddTicket.page";
+import { TicketLists } from "./pages/ticket-list/TicketLists.page";
 function App() {
   return (
     <div className="App">
@@ -11,6 +12,7 @@ function App() {
       <DefaultLayout>
         <Dashboard />
         <AddTicket />
+        <TicketLists />
       </DefaultLayout>
     </div>
   );
