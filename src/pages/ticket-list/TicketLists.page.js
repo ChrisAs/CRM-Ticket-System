c0d89f46ab9c7437d01f334554d4db511e12010a
Chris <zari87@live.com>
2020-11-03 00:24:29 -0500

    Added: Ticket Table

--- src/pages/ticket-list/TicketLists.page.js
+++ src/pages/ticket-list/TicketLists.page.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { Button, Container, Row, Col, PageBreadcrumb } from "react-bootstrap";
 import { SearchForm } from "../../components/search-form/SearchForm.comp";
+import { TicketTable } from "../../components/ticket-table/TicketTable.comp";
+import tickets from "../../assets/data/dummy-tickets.json";
 export const TicketLists = () => {
-  const [str, setStr] = useState("")
-  ;
+  const [str, setStr] = useState("");
   useEffect(() => {}, [str]);
 
   const handleOnChange = (e) => {
@@ -22,6 +23,12 @@ export const TicketLists = () => {
         </Col>
         <Col className="text-right">Search Form</Col>
         <SearchForm handleOnChange={handleOnChange} str={str} />
+      </Row>
+      <hr />
+      <Row>
+        <Col>
+          <TicketTable tickets={tickets} />
+        </Col>
       </Row>
     </Container>
   );
