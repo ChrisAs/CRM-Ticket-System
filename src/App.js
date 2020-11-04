fa248e52db0e3fbb02aacd8125161ea505ba25c3
Chris <zari87@live.com>
2020-11-03 23:42:58 -0500

    Added: ticket subject information

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Dashboard } from "./pages/dashboard/Dasboard.page";
 import { DefaultLayout } from "./layout/DefaultLayout";
 import { AddTicket } from "./pages/new-ticket/AddTicket.page";
 import { TicketLists } from "./pages/ticket-list/TicketLists.page";
+import { Ticket } from "./pages/ticket/Ticket.page";
 function App() {
   return (
     <div className="App">
@@ -13,6 +14,7 @@ function App() {
         <Dashboard />
         <AddTicket />
         <TicketLists />
+        <Ticket />
       </DefaultLayout>
     </div>
   );
--- src/pages/ticket/Ticket.page.js
+++ src/pages/ticket/Ticket.page.js
@@ -1,14 +1,26 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import { PageBreadcrumb } from "../../components/breadcrumb/Breadcrumb.comp";
+import tickets from "../../assets/data/dummy-tickets.json";
 
+const ticket = ticket[0];
 export const Ticket = () => {
   return (
     <div>
       <Container>
         <Row>
           <Col>
-            <PageBreadcrumb page={Ticket} />
+            <PageBreadcrumb page="Ticket" />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div className="subject">Subject is:{ticket.subject}</div>
+            <div className="date">Subject is:{ticket.addedAt}</div>
+            <div className="status">Subject is:{ticket.status}</div>
+          </Col>
+          <Col>
+            <Button variant="outline-info">Close Ticket</Button>
           </Col>
         </Row>
       </Container>
