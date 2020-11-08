5eb7f6717d10a132a96bd9312e32bd0ea548b4db
Chris <zari87@live.com>
2020-11-07 21:56:01 -0500

    Updated: Nav.Links to Links

--- src/layout/partials/Header.comp.js
+++ src/layout/partials/Header.comp.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Navbar, Nav, NavbarBrand } from "react-bootstrap";
 import logo from "../../assets/img/logo.png";
+import { Link } from "react-router-dom";
 export const Header = () => {
   return (
     <Navbar collapseOnSelect bg="info" variant="dark" expand="md">
@@ -10,9 +11,12 @@ export const Header = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
-          <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+          <Link to="/dashboard">Dashboard</Link>
+          <Link to="/dashboard">Tickets</Link>
+          <Link to="/dashboard">Logout</Link>
+          {/* <Nav.Link href="/dashboard">Dashboard</Nav.Link>
           <Nav.Link href="/dashboard">Tickets</Nav.Link>
-          <Nav.Link href="/dashboard">Logout</Nav.Link>
+          <Nav.Link href="/dashboard">Logout</Nav.Link> */}
         </Nav>
       </Navbar.Collapse>
     </Navbar>
