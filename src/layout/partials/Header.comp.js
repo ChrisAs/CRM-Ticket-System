102af383b923bfcba66c860bc906293c35844354
Chris <zari87@live.com>
2020-11-08 17:21:45 -0500

    Update: Header comp logmeout/link tags

--- src/layout/partials/Header.comp.js
+++ src/layout/partials/Header.comp.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { Navbar, Nav, NavbarBrand } from "react-bootstrap";
 import logo from "../../assets/img/logo.png";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
 export const Header = () => {
+  const history = useHistory();
+
+  const logMeOut = () => {
+    history.push("/");
+  };
   return (
     <Navbar collapseOnSelect bg="info" variant="dark" expand="md">
       <Navbar.Brand>
@@ -11,12 +17,15 @@ export const Header = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
-          <Link to="/dashboard">Dashboard</Link>
-          <Link to="/dashboard">Tickets</Link>
-          <Link to="/dashboard">Logout</Link>
-          {/* <Nav.Link href="/dashboard">Dashboard</Nav.Link>
-          <Nav.Link href="/dashboard">Tickets</Nav.Link>
-          <Nav.Link href="/dashboard">Logout</Nav.Link> */}
+          <LinkContainer>
+            <Nav.Link to="/dashboard">Dashboard</Nav.Link>
+          </LinkContainer>
+          <LinkContainer>
+            <Nav.Link to="/tickets">Tickets</Nav.Link>
+          </LinkContainer>
+          <LinkContainer>
+            <Nav.Link onClick={logMeOut}>Logout</Nav.Link>
+          </LinkContainer>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
