f00a16c5ed8c470c954c65fbac52fb54898300a5
Chris <zari87@live.com>
2020-11-15 01:04:42 -0500

    Added http file

--- server/app.js
+++ server/app.js
@@ -7,40 +7,53 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const port = process.env.PORT || 3001;
 
+//API security
+app.use(helmet());
+
+//handle CORS error
 app.use(cors());
 
 //MongoDB Connection Setup
+const mongoose = require("mongoose");
 
-// const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
 
-// mongoose.connect(process.env.MONGO_URL, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useFindAndModify: false,
-//   useCreateIndex: true,
-// });
+if (process.env.NODE_ENV !== "production") {
+  const mDb = mongoose.connection;
+  mDb.on("open", () => {
+    console.log("MongoDB is conneted");
+  });
 
-// if (process.env.NODE_ENV !== "production") {
-//     const mDb = mongoose.connection;
-//     mDb.on("open", () => {
-//       console.log("MongoDB is conneted");
-//     });
+  mDb.on("error", (error) => {
+    console.log(error);
+  });
 
-// mDb.on("error", (error) => {
-//     console.log(error);
-//   });
+  //Logger
+  app.use(morgan("tiny"));
+}
 
-//   Logger --
-//   app.use(morgan("tiny"));
-// }
-const userRouter = require("./src/routers/user.router");
-
-app.use("/v1/user", userRouter);
-// app.use("/v1/ticket", ticketRouter);
-// app.use("/v1/tokens", tokensRouter);
+// Set body bodyParser
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+//Load routers
+const userRouter = require("./src/routers/user.router");
+const ticketRouter = require("./src/routers/ticket.router");
+const tokensRouter = require("./src/routers/tokens.router");
+
+//Use Routers
+app.use("/v1/user", userRouter);
+app.use("/v1/ticket", ticketRouter);
+app.use("/v1/tokens", tokensRouter);
+
+//Error handler
+const handleError = require("./src/utils/errorHandler");
 
 app.use((req, res, next) => {
   const error = new Error("Resources not found!");
@@ -48,13 +61,10 @@ app.use((req, res, next) => {
   next(error);
 });
 
-//Error handler
-const handleError = require("./src/utils/errorHandler");
-
 app.use((error, req, res, next) => {
   handleError(error, res);
 });
 
 app.listen(port, () => {
-  console.log(`Server is ready on http://localhost:${port}`);
+  console.log(`API is ready on http://localhost:${port}`);
 });
