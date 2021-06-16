c0efb3cbb0a484da7675a40d1977ddfc1ba7d360
Chris <zari87@live.com>
2021-06-15 22:18:13 -0400

    Added: June 15 update

--- src/layout/partials/Footer.comp.js
+++ src/layout/partials/Footer.comp.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Footer = () => {
   return (
     <div className="text-center copy-right">
-      &copy; CRM all rights reserved - 2020
+      &copy; CRM all rights reserved - 2021
     </div>
   );
 };
--- src/layout/partials/Header.comp.js
+++ src/layout/partials/Header.comp.js
@@ -4,12 +4,18 @@ import logo from "../../assets/img/logo.png";
 import { useHistory } from "react-router-dom";
 import { LinkContainer } from "react-router-bootstrap";
 
+import { userLogout } from "../../api/userApi";
+
 export const Header = () => {
   const history = useHistory();
 
   const logMeOut = () => {
+    sessionStorage.removeItem("accessJWT");
+    localStorage.removeItem("crmSite");
+    userLogout();
     history.push("/");
   };
+
   return (
     <Navbar collapseOnSelect bg="info" variant="dark" expand="md">
       <Navbar.Brand>
