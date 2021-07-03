a9b3d9f64869d2f9f8788d7c1bde560c7926c137
Chris <zari87@live.com>
2021-07-02 23:44:45 -0400

    July Update

--- src/components/search-form/SearchForm.comp.js
+++ src/components/search-form/SearchForm.comp.js
@@ -1,29 +1,34 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { filterSerachTicket } from "../../pages/ticket-list/ticketsAction";
+
 import { Form, Row, Col } from "react-bootstrap";
-import PropTypes from "prop-types";
-export const SearchForm = ({ handleOnChange, str }) => {
+
+export const SearchForm = () => {
+  const dispatch = useDispatch();
+
+  const handleOnChange = (e) => {
+    const { value } = e.target;
+
+    dispatch(filterSerachTicket(value));
+  };
+
   return (
     <div>
       <Form>
         <Form.Group as={Row}>
           <Form.Label column sm="3">
-            Search{" "}
+            Search:
           </Form.Label>
           <Col sm="9">
             <Form.Control
               name="searchStr"
               onChange={handleOnChange}
-              value={str}
               placeholder="Search ..."
-            ></Form.Control>
+            />
           </Col>
         </Form.Group>
       </Form>
     </div>
   );
-};
-
-SearchForm.propTypes = {
-  handleOnChange: PropTypes.func.isRequired,
-  str: PropTypes.string.isRequired,
 };
